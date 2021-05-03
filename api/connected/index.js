f96b0b2ccda2b5d6faee758c8ffbb7e34ac3fd6b
Ben Coleman <benc.uk@gmail.com>
2021-05-03 10:34:11 +0100

    functions work

--- api/connected/index.js
+++ api/connected/index.js
@@ -1,4 +1,6 @@
 module.exports = async function (context, connectionContext) {
+  console.log('############## CONNECTED')
+  console.log(context)
   // !IMPORTANT! Without this, nothing will work and you will see no errors :/
   return {
     roles: ['webpubsub.sendToGroup', 'webpubsub.joinLeaveGroup'],
